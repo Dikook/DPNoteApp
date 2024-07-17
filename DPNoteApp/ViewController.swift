58ffae5b8f00047dad72e7eb0e0e28634fe29509
Dikook <vimvom931@gmail.com>
2024-07-17 23:39:27 +0500

    Initial Commit

---
+++ DPNoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DPNoteApp
+//
+//  Created by Diar Orynbek on 17.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
